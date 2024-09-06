5873b1cd37daba4a7de2f0095ed17a9a7d206c74
sherif <secit91@gmail.com>
2024-09-06 21:15:18 +0300

    first commit

---
+++ lib/app/data/APP_variables/payment_releted.dart
@@ -0,0 +1,16 @@
+bool sandboxVerifyReceiptUrl = true;
+
+/// ====== StripePey ======== \\\
+//String stripPublishableKey =
+  //  "pk_test_51IWywoSGE6fS01MG9U0usNXTW7BufqX1o2eFV4xAWSdTmZhrA7ixXV8EivBuGExwKZQHwH0hHtRvA2ASflY0fGIR00ol26LdEQ";
+
+//String stripSecrateKey =
+//  "sk_test_51IWywoSGE6fS01MGf7xcpOZUnrvBGg7oqzz5HSquArnaft5JWVNX860dJSWrZbP9x2SQs7huXP8Okso2Wjkhcv0i00we2AcFVx";
+bool stripActive = true;
+String stripPublishableKey = "pk_test_51OZQX5AhTWDvQvHlfkRsBZNl7Kq1NIo9Ua1CL2qUCZSd9SjEHQJvrXRd03nTicEbvf7Mc5VfNt9EikbPaMiiwx9100xJvqsVL7";
+String stripSecrateKey = "sk_test_51OZQX5AhTWDvQvHldJrp8MahnPcPZ0VsyBmoK3JuxAlval1rr6mpSH90Xl1fASQbcDdUAwmiCJrfrRt9xSYh9OAB00SkorS0Zf";
+
+/// ====== RazorPay ======= \\\\
+//String razorPayKey = "rzp_test_NNbwJ9tmM0fbxj";
+bool razorPayActive = true;
+String razorPayKey = "";
